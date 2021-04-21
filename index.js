16c429853185f08996bd54e66b06e4d3c1d7157e
Dhruv Doshi <dhruvdoshi@gmail.com>
2021-04-22 00:06:24 +0530

    search route working

--- index.js
+++ index.js
@@ -7,26 +7,30 @@ const PORT = process.env.PORT || 5000
 const express = require('express');
 const app = express()
 
-// Use body-parser
+// Use body-parser to enable requests to come in the body
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-const filePath = path.join(__dirname, './data.json');
+const filePath = path.join(__dirname, './data.json'); 
 
 app.get('/search', (req, res) => {
-    let keyword =  req.query.query;
+    let keyword = req.query.query.toLowerCase();
+    keyword = keyword.replace(/"/g, '') //we have to remove the double quotes as node.js identifies "wall" and wall differently 
 
     let searchResult = []
     let count = 0
 
-    let rawdata = fs.readFileSync(filePath);
-    let searchData = JSON.parse(rawdata);
+    let searchData = JSON.parse(fs.readFileSync(filePath)); 
 
-    for(let i =0; i<searchData.length ; i=i+1) {
-        let check = searchData[i].text.toLowerCase();
-        console.log(check)
+    for(let i = 0; i<searchData.length ; i=i+1) {
+        
+        let check = []
+        check = searchData[i].text.toLowerCase()
+        check = check.split(' ');
+        
         if(check.includes(keyword)) {
+
             count = count + 1
             searchResult.push({
                 id: searchData[i].id,
